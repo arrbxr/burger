26e9c93f35282ad8e78a14f0357d1458db42e67e
arrbxr <arrbxr@gmail.com>
2019-09-30 04:43:27 +0530

    Updates

---
+++ precache-manifest.f78220782c7b973a25e67b0ada0dd233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67feb880fb3d94b962b0abdc018eb2a3",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "f96bc40201b5f3fb4578",
+    "url": "/burger/static/css/main.b89fb6c2.chunk.css"
+  },
+  {
+    "revision": "09c5668605ea6abde338",
+    "url": "/burger/static/js/2.ddde1600.chunk.js"
+  },
+  {
+    "revision": "f96bc40201b5f3fb4578",
+    "url": "/burger/static/js/main.b4bb591a.chunk.js"
+  },
+  {
+    "revision": "5eb730d2b9a6ed352ca7",
+    "url": "/burger/static/js/runtime-main.9873e49b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
